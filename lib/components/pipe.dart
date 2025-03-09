c163d2d1558d226add48da0b94ac1025510a4235
islomhon <ilhomjonovislomhon59@gmail.com>
2025-03-09 12:17:31 +0500

    Pipelarni harakatlanadigan qilib qoydim

--- lib/components/pipe.dart
+++ lib/components/pipe.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flappy_bird/constans.dart';
@@ -8,44 +8,72 @@ import 'package:flappy_bird/game.dart';
 class Pipe extends SpriteComponent
     with CollisionCallbacks, HasGameRef<FlappyBird> {
   final bool isTopPipe;
-
-  //score 
   bool scored = false;
+  double heightChangeSpeed = 60; // Ustun uzunligi o'zgarish tezligi
+  double minHeight = 50;
+  double maxHeight;
+  bool movingDown = true; // Yo'nalish
 
-
-
-  //init
   Pipe(Vector2 position, Vector2 size, {required this.isTopPipe})
-      : super(
+      : maxHeight = size.y,
+        super(
           position: position,
           size: size,
         );
 
-
-  //LOAD
   @override
   FutureOr<void> onLoad() async {
-    //load sprite image
     sprite = await Sprite.load(isTopPipe ? 'pipe.png' : 'pipe-green.png');
-    //add hitbox
     add(RectangleHitbox());
   }
-  
 
-  //UPDATE
   @override
   void update(double dt) {
-    // check if the bird has passed this pipe
-    if(!scored && position.x + size.x < gameRef.bird.position.x){
+    if (!scored && position.x + size.x < gameRef.bird.position.x) {
       scored = true;
-      if(isTopPipe){
+      if (isTopPipe) {
         gameRef.incrementScore();
       }
     }
-    //scrool pipe to left
+
+    if (movingDown) {
+      if (isTopPipe) {
+        size.y += heightChangeSpeed * dt; // Yuqori pipe pastga harakat qiladi
+        if (size.y > maxHeight) {
+          size.y = maxHeight;
+          movingDown = false; // Yo‘nalishni o‘zgartirish
+        }
+      } else {
+        double newHeight = size.y - heightChangeSpeed * dt;
+        if (newHeight > minHeight) {
+          position.y += heightChangeSpeed * dt; // Faqat yuqori chizig‘ini harakatlantirish
+          size.y = newHeight;
+        } else {
+          size.y = minHeight;
+          movingDown = false; // Yo‘nalishni o‘zgartirish
+        }
+      }
+    } else {
+      if (isTopPipe) {
+        size.y += heightChangeSpeed * dt; // Yuqori pipe tepaga harakat qiladi
+        if (size.y < minHeight) {
+          size.y = minHeight;
+          movingDown = true; // Yo‘nalishni o‘zgartirish
+        }
+      } else {
+        double newHeight = size.y + heightChangeSpeed * dt;
+        if (newHeight < maxHeight) {
+          position.y -= heightChangeSpeed * dt; // Faqat yuqori chizig‘ini harakatlantirish
+          size.y = newHeight;
+        } else {
+          size.y = maxHeight;
+          movingDown = true; // Yo‘nalishni o‘zgartirish
+        }
+      }
+    }
+
     position.x -= groundScroolingSpeed * dt;
-    //remove pipe
-    if(position.x + size.x <= 0){
+    if (position.x + size.x <= 0) {
       removeFromParent();
     }
   }
